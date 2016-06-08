013c61db58f69f390a2a81cbe2643b2ed6ec96ae
Miloš Kočan <milos.kocan@akademiasovy.sk>
2016-06-08 09:08:34 +0200

    Added more maps positions

--- app/src/main/java/com/pmm/triedenieodpadukosice/MapsActivity.java
+++ app/src/main/java/com/pmm/triedenieodpadukosice/MapsActivity.java
@@ -38,18 +38,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng latlng1 = new LatLng(48.734132, 21.283426); //Lidické nám.16 - kosice
+        LatLng latlng1 = new LatLng(48.734132, 21.283426); //Lidické nám.16 - Kosice
+        LatLng latLng2 = new LatLng(48.702963, 21.254920); //Krakovska 4 - Kosice
+        LatLng latLng3 = new LatLng(48.715944, 21.218213); //Wuppertálska 21 - Kosice
+        LatLng latLng4 = new LatLng(48.718280, 21.217435); //Titogradská 17 - Kosice
+        LatLng latLng5 = new LatLng(48.697621, 21.281691); //Bukovecká 1 - Kosice
+        LatLng latLng6 = new LatLng(48.756893, 21.265934); //Bruselská 4 - 5 - Kosice
 
 
         CameraUpdate camera = CameraUpdateFactory.newLatLngZoom(latlng1, 10);
         mMap.moveCamera(camera);
 
-        MarkerOptions options = new MarkerOptions()
+        MarkerOptions options1 = new MarkerOptions()
                 .position(latlng1)
                 .title("29.4.- 6.5 - Jaltská 23")
                 .snippet("Kontajner zo zadnejstrany bloku");
 
-        mMap.addMarker(options).showInfoWindow();
+        MarkerOptions options2 = new MarkerOptions()
+                .position(latLng2)
+                .title("8.7.-15.7 - Krakovská 4")
+                .snippet("Garáže");
+
+        MarkerOptions options3 = new MarkerOptions()
+                .position(latLng3)
+                .title("15.7.- 22.7 - Wuppertálska 21");
+
+        MarkerOptions options4 = new MarkerOptions()
+                .position(latLng4)
+                .title("12.8.- 19.8 - Titogradská 17");
+
+        MarkerOptions options5 = new MarkerOptions()
+                .position(latLng5)
+                .title("1.7.- 4.7 - Bukovecká 1");
+
+        MarkerOptions options6 = new MarkerOptions()
+                .position(latLng6)
+                .title("5.7.- 12.7 - Bruselská 4 - 5");
+
+
+        mMap.addMarker(options1).hideInfoWindow();
+        mMap.addMarker(options2).hideInfoWindow();
+        mMap.addMarker(options3).hideInfoWindow();
+        mMap.addMarker(options4).hideInfoWindow();
+        mMap.addMarker(options5).hideInfoWindow();
+        mMap.addMarker(options6).hideInfoWindow();
 
 
 
